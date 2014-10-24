376daec31851b4c079629d8461d2ef7553aa6d9f
mamod <mamod.mehyar@gmail.com>
2014-10-24 23:52:03 +0300

    dummy clean

--- make.pl
+++ make.pl
@@ -4,9 +4,9 @@ my $buildStr = "gcc"
              . " -I./include"
              . " -L."
              . " include/duktape/duktape.c"
-             . " src/loop/core.c"
              . " include/thread/tinycthread.c"
              . " include/http/http_parser.c"
+             . " src/loop/core.c"
              . " -o como" . ($isWin ? ".exe" : "");
             if ($isWin){
                 $buildStr .= " include/ansi/ANSI.c"
