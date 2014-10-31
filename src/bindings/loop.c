5523e24b613f02e786d8e2a55f2e83e5b592a6e1
mamod <mamod.mehyar@gmail.com>
2014-10-31 05:01:56 +0300

    code clean + handle_wrap & unwrap

--- src/bindings/loop.c
+++ src/bindings/loop.c
@@ -2,23 +2,10 @@
 
 typedef struct comoLoopHandle {
     duk_context *ctx;
-    void *self;
+    void *wrap;
     void *cb;
+    void *close_cb;
 } comoLoopHandle;
-
-static void dump_stack2(duk_context *ctx, const char *name) {
-    duk_idx_t i, n;
-    n = duk_get_top(ctx);
-    printf("%s (top=%ld):", name, (long) n);
-    for (i = 0; i < n; i++) {
-        printf(" ");
-        duk_dup(ctx, i);
-        printf("%s", duk_safe_to_string(ctx, -1));
-        duk_pop(ctx);
-    }
-    printf("\n");
-    fflush(stdout);
-}
 
 static int _dispatch_cb (evHandle *handle, int mask){
     comoLoopHandle *data = handle->data;
@@ -33,10 +20,32 @@ static int _dispatch_cb (evHandle *handle, int mask){
         duk_call(ctx, 1);
     } else {
         duk_push_pointer(ctx, handle);
-        duk_call(ctx, 1);    
+        duk_call(ctx, 1);
     }
 
     duk_pop_2(ctx);
+    //dump_stack(ctx, "LOOP");
+    return 1;
+}
+
+static int _close_cb (evHandle *handle){
+    comoLoopHandle *data = handle->data;
+    duk_idx_t idx_top;
+    if (!data){
+        return 1;
+    }
+
+    duk_context *ctx = data->ctx;
+    void *close_cb   = data->close_cb;
+    if (close_cb){
+        duk_push_global_object(ctx);
+        duk_push_object_pointer(ctx, close_cb);
+        duk_push_pointer(ctx, handle);
+        //FIXME
+        //duk_call(ctx, 1);
+        idx_top = duk_get_top_index(ctx);
+        duk_pop_n(ctx, idx_top + 1);
+    }
     //dump_stack(ctx, "LOOP");
     return 1;
 }
@@ -54,7 +63,9 @@ static const int _loop_init_loop(duk_context *ctx) {
 }
 
 static const int _loop_unref(duk_context *ctx) {
-    
+    evHandle *handle = duk_require_pointer(ctx, 0);
+    handle_unref(handle);
+    return 1;
 }
 
 static const int _loop_run(duk_context *ctx) {
@@ -115,16 +126,45 @@ static const int _loop_update_time(duk_context *ctx) {
     return 1;
 }
 
+static const int _loop_handle_wrap(duk_context *ctx) {
+    evHandle *handle = duk_require_pointer(ctx, 0);
+    void *wrap       = duk_to_pointer(ctx, 1);
+
+    comoLoopHandle *data = handle->data;
+    data->wrap = wrap;
+
+    return 1;
+}
+
+static const int _loop_handle_unwrap(duk_context *ctx) {
+    evHandle *handle = duk_require_pointer(ctx, 0);
+    comoLoopHandle *data = handle->data;
+    duk_push_object_pointer(ctx, data->wrap);
+    return 1;
+}
+
+static const int _loop_close(duk_context *ctx) {
+    evHandle *handle = duk_require_pointer(ctx, 0);
+    void *close_cb   = duk_to_pointer(ctx, 1);
+    comoLoopHandle *data = handle->data;
+    data->close_cb = close_cb;
+    loop_close(handle, _close_cb);
+    return 1;
+}
+
 static const duk_function_list_entry loop_funcs[] = {
     { "init_loop", _loop_init_loop, 0 },
     { "default_loop", _loop_default_loop, 0},
     { "init_handle", _loop_init_handle, 2 },
-    {"timer_start", _loop_timer_start, 4},
-    {"io_start", _loop_io_start, 3 },
-    {"io_stop", _loop_io_stop, 2   },
+    { "timer_start", _loop_timer_start, 4},
+    { "io_start", _loop_io_start, 3 },
+    { "io_stop", _loop_io_stop, 2   },
     { "run_loop", _loop_run, 2 },
-    {"update_time", _loop_update_time, 1},
-    { "unref", _loop_unref, 2 },
+    { "update_time", _loop_update_time, 1},
+    { "unref", _loop_unref, 1 },
+    { "handle_wrap", _loop_handle_wrap, 2},
+    { "handle_unwrap", _loop_handle_unwrap, 1},
+    { "close", _loop_close,   2},
     { NULL, NULL, 0 }
 };
 
