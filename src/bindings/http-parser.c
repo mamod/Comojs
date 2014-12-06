ade08d4fbfb31753d75b3b4fe38d7331c92688e2
mamod <mamod.mehyar@gmail.com>
2014-12-06 04:02:17 +0300

    pop stack properly, add more methods.. still need tests

--- src/bindings/http-parser.c
+++ src/bindings/http-parser.c
@@ -19,7 +19,8 @@ do { \
         duk_push_lstring(ctx, "", 0); \
     }\
     duk_call_method(ctx, 1);\
-    duk_pop(ctx);\
+    duk_idx_t idx_top = duk_get_top_index(ctx);\
+    duk_pop_n(ctx, idx_top + 1);\
     /*dump_stack(ctx, "PARSER");*/ \
     return duk_get_int(ctx, -1);\
 } while(0)
@@ -93,6 +94,11 @@ static const int _http_parser_init(duk_context *ctx) {
     return 1;
 }
 
+size_t _http_parser_parse_thread (http_parser *p, const char *str, size_t len) {
+    size_t nparsed = http_parser_execute(p, &settings_, str, len);
+    return nparsed;
+}
+
 static const int _http_parser_parse(duk_context *ctx) {
     http_parser *p  = duk_require_pointer(ctx, 0);
     const char *str = duk_get_string(ctx, 1);
@@ -110,6 +116,57 @@ static const int _http_parser_reinitialize(duk_context *ctx) {
     return 1;
 }
 
+static const int _http_should_keep_alive(duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    int ret = http_should_keep_alive(p);
+    if (ret == 0){
+        duk_push_false(ctx);
+    } else {
+        duk_push_true(ctx);
+    }
+    return 1;
+}
+
+static const int _http_minor(duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    int ret = p->http_minor;
+    duk_push_int(ctx, ret);
+    return 1;
+}
+
+static const int _http_major(duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    int ret = p->http_major;
+    duk_push_int(ctx, ret);
+    return 1;
+}
+
+static const int _http_method(duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    int ret = p->method;
+    duk_push_int(ctx, ret);
+    return 1;
+}
+
+static const int _http_upgrade (duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    int ret = p->upgrade;
+    if (ret == 0){
+        duk_push_false(ctx);
+    } else {
+        duk_push_true(ctx);
+    }
+    return 1;
+}
+
+static const int _http_parser_destroy(duk_context *ctx) {
+    http_parser *p  = duk_require_pointer(ctx, 0);
+    comoHttpParser *data = p->data;
+    free(data);
+    free(p);
+    return 1;
+}
+
 static const duk_number_list_entry como_http_parser_constants[] = {
     {"HTTP_REQUEST"  , HTTP_REQUEST},
     {"HTTP_RESPONSE" , HTTP_RESPONSE},
@@ -121,6 +178,12 @@ static const duk_function_list_entry como_http_parser_funcs[] = {
     { "init"          , _http_parser_init, 2 },
     { "parse"         , _http_parser_parse, 3 },
     { "reinitialize"  , _http_parser_reinitialize, 2 },
+    { "http_should_keep_alive"  , _http_should_keep_alive, 1 },
+    { "http_minor"    , _http_minor, 1 },
+    { "http_major"    , _http_major, 1 },
+    { "http_upgrade"  , _http_upgrade, 1 },
+    { "http_method"   , _http_method, 1 },
+    { "destroy"       , _http_parser_destroy, 1 },
     { NULL            , NULL, 0 }
 };
 
