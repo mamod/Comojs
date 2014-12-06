6be041abb6821479175d174b5145f1d5bdbe424f
mamod <mamod.mehyar@gmail.com>
2014-12-06 03:57:56 +0300

    fix indentation

--- include/ansi/ANSI.h
+++ include/ansi/ANSI.h
@@ -1,16 +1,16 @@
 BOOL
 WINAPI MyWriteConsoleA( HANDLE hCon, LPCVOID lpBuffer,
-			DWORD nNumberOfCharsToWrite,
-			LPDWORD lpNumberOfCharsWritten, LPVOID lpReserved );
+            DWORD nNumberOfCharsToWrite,
+            LPDWORD lpNumberOfCharsWritten, LPVOID lpReserved );
 
 BOOL
 WINAPI MyWriteConsoleW( HANDLE hCon, LPCVOID lpBuffer,
-			DWORD nNumberOfCharsToWrite,
-			LPDWORD lpNumberOfCharsWritten, LPVOID lpReserved );
+            DWORD nNumberOfCharsToWrite,
+            LPDWORD lpNumberOfCharsWritten, LPVOID lpReserved );
 
 BOOL
 WINAPI MyWriteFile( HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite,
-		    LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped );
+            LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped );
 
 void tty_console_init();
 void tty_enter_critical_section();
